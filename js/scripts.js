2b5f657c9509473f00e995dc7266ea6bc1fb53fc
Kaila Sprague McRae <kaila.sprague@icloud.com>
2020-11-03 13:01:22 -0800

    this will add UI Logic
    Co-authored-by:Ryland Adams <rylandadams@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -41,21 +41,78 @@ AddressBook.prototype.deleteContact = function(id) {
 
 
 //Logic for Contacts--------
-function Contact(firstName, lastName, phoneNumber) {
+function Address(workAddress, personalAddress){
+  this.workAddress = workAddress;
+  this.personalAddress = personalAddress;
+}
+
+Address.prototype.addAddress = function() {
+  this.address.push(contact);
+}
+
+function Contact(firstName, lastName, phoneNumber, email, address) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
-}
+  this.email = email;
+  this.address = Address; 
+  }
+
 
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
 
-let addressBook = new AddressBook();
-let contact = new Contact("Ada", "Lovelace", "503-555-0100");
-let contact2 = new Contact ("Grace", "Hopper", "503-555-0199");
-
-addressBook.addContact(contact);
-addressBook.addContact(contact2);
-
 //user interface logic
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo= "";
+  addressBookToDisplay.contacts.forEach(function(contact){
+    htmlForContactInfo += "<li id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+}
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId); 
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  $(".email").html(contact.email);
+  $(".address").html(contact.address);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class= 'deleteButton' id=" + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function(){
+    showContact(this.id);
+  });
+  $("#buttons").on("click", ".deleteButton", function(){
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+
+$(document).ready(function(){
+  attachContactListeners();
+  $("form#new-contact").submit(function(event){
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    const inputtedEmail = $("input#new-email").val();
+    const inputtedWorkAddress = $("input#new-workAddress").val();
+    const inputtedPersonalAddress = $("input#new-personalAddress").val();
+    let newAddress = new Address (inputtedWorkAddress, inputtedPersonalAddress)
+    let newContact = new Contact (inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmail, newAddress);
+    addressBook.addContact(newContact);
+    newContact.addAddress(newAddress);
+    displayContactDetails(addressBook);
+  });
+});
